aa9037bf094be9c88e7b6d7b3151c14e65e17179
gampolt <gampolt@gmail.com>
2019-06-12 10:00:44 +0700

    add sass

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,12 @@
 <script>
-	export let name;
+  export let name;
 </script>
 
-<style>
-	h1 {
-		color: purple;
-	}
+<style lang="sass">
+  $blue: #00f;
+  h1 {
+    color: $blue;
+  }
 </style>
 
 <h1>Hello {name}!</h1>
